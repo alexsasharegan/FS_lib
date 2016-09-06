66fde1d0af90b98258a68c5c86de81fa97e4fce9
Alex Regan <alex.regan@artisancolour.com>
2016-09-05 21:16:50 -0700

    switched to using 'src' directory

--- FS_Autoloader.php
+++ FS_Autoloader.php
@@ -1,11 +1,13 @@
 <?php
+
 function FS_Autoloader($classname) {
   $pathParts = explode("\\", $classname);
-  array_unshift($pathParts, 'lib');
+  array_unshift($pathParts, 'src');
   $path = implode(DIRECTORY_SEPARATOR, $pathParts);
   $filename = __DIR__.DIRECTORY_SEPARATOR.$path.'.class.php';
   if (is_readable($filename)) {
     require_once $filename;
   }
 }
+
 spl_autoload_register('FS_Autoloader', true);
